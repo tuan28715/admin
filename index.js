0b3bfb07c2d3c8388e6eeeec2982db5b513646d6
Pham Tran Duc Trong <trong.ptd3604@sinhvien.hoasen.edu.vn>
2021-10-03 20:00:35 +0700

    heroku

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + '/admin'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'admin', 'index.html'))
+const express = require('express'); 
+const server = express(); 
+const cors = require('cors');
+server.use(express.json());
+server.use(cors());
+
+
+// Serve only the static files form the dist directory
+server.use(express.static(__dirname + '/dist/admin'));
+
+server.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/admin/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080,()=>console.log(`server is running !`));
+
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Your app running on port 3000");
+})
